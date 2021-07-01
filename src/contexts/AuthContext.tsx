8a580a1f1ffd66af42d56867b7e92dbe5ca536e9
ManassesM <manamiui2001@gmail.com>
2021-07-01 10:50:50 -0300

    Aula 4

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,36 +1,33 @@
-
-import { createContext, ReactNode, useEffect, useState } from "react"
-import { auth, firebase } from "../services/firebase"
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { auth, firebase } from "../services/firebase";
 
 type User = {
-  id: string,
-  name: string,
-  avatar: string
+  id: string;
+  name: string;
+  avatar: string;
 }
 
 type AuthContextType = {
-  user: User | undefined,
-  signInWithGoogle: () => Promise<void>
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
 }
 
 type AuthContextProviderProps = {
-  children: ReactNode
+  children: ReactNode;
 }
 
-export const AuthContext = createContext({} as AuthContextType)
+export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthContextProvider(props: AuthContextProviderProps) {
-  const [user, setUser] = useState<User>()
+  const [user, setUser] = useState<User>();
 
-  // watching login to get users information if the page gets reloaded
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       if (user) {
-
         const { displayName, photoURL, uid } = user
 
         if (!displayName || !photoURL) {
-          throw new Error('Missing information from Google Account.')
+          throw new Error('Missing information from Google Account.');
         }
 
         setUser({
@@ -42,21 +39,20 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
     })
 
     return () => {
-      unsubscribe()
+      unsubscribe();
     }
-  }, [user])
+  }, [])
 
-  // using firebase to connect to our google account
   async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider()
+    const provider = new firebase.auth.GoogleAuthProvider();
 
-    const result = await auth.signInWithPopup(provider)
+    const result = await auth.signInWithPopup(provider);
 
     if (result.user) {
       const { displayName, photoURL, uid } = result.user
 
       if (!displayName || !photoURL) {
-        throw new Error('Missing information from Google Account.')
+        throw new Error('Missing information from Google Account.');
       }
 
       setUser({
@@ -65,14 +61,11 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
         avatar: photoURL
       })
     }
-
   }
-
+  
   return (
-
     <AuthContext.Provider value={{ user, signInWithGoogle }}>
       {props.children}
     </AuthContext.Provider>
-
-  )
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,20 @@
-
 import { useHistory } from "react-router-dom";
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { database } from "../services/firebase";
 import { Button } from '../components/Button'
 import { useAuth } from "../hooks/useAuth";
 
 import '../styles/auth.scss'
+import { FormEvent, useState } from "react";
 
 export function Home() {
   const history = useHistory()
   const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode ] = useState('')
 
   async function handleCreateRoom() {
     if (!user) {
@@ -21,7 +23,20 @@ export function Home() {
 
     history.push('/rooms/new')
   }
+  
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
 
+    
+    if(roomCode.trim() === '') return
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    !roomRef.exists()
+      ? alert('Room does not exist') 
+      : history.push(`/rooms/${roomCode}`)
+  }
+
+  // COMPONENT
   return (
     <div id="page-auth">
 
@@ -41,10 +56,12 @@ export function Home() {
           </button>
 
           <div className="separator">Ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={e => setRoomCode(e.target.value)}
+              defaultValue={roomCode}
             />
             <Button type="submit">
               Entrar na sala
